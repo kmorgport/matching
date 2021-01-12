ea4f580a979a1619ed4b96561ea18554ea4e898a
Kole Morgan <kmorgan0057@gmail.com>
2021-01-12 15:13:31 -0600

    card images testing

--- js/matching.js
+++ js/matching.js
@@ -1,7 +1,16 @@
 "use strict"
-const letters = 'data/matching.json'
+const letters = 'data/greekmatch.json'
 const pokemon = "https://pokeapi.co/api/v2/pokemon/"
-
+function addNumbers(stuff){
+    console.log(stuff[1].id + stuff[2].id)
+}
+function writeDoc(stuff){
+    let item = stuff[1].id
+    document.open();
+    document.write('<h1>'+item+'</h1>')
+    document.close()
+}
+// writeDoc(data)
 fetch(letters)
     .then(data=>data.json())
-    .then(data=>console.log(data));
+    .then(data=> console.log(data));
