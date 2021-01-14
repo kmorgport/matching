587df93babdc87555109a25798fa9513b245663a
Kole Morgan <kmorgan0057@gmail.com>
2021-01-13 19:53:22 -0600

    Random Deck Creation Logic Complete

--- js/matching.js
+++ js/matching.js
@@ -1,5 +1,5 @@
 "use strict"
-const letters = 'data/greekmatch.json'
+const letters = 'data/abjadmatch.json'
 const pokemon = "https://pokeapi.co/api/v2/pokemon/"
 function addNumbers(stuff){
     console.log(stuff[1].id + stuff[2].id)
@@ -13,13 +13,13 @@ function writeDoc(stuff){
 // writeDoc(data)
 
 const row = document.getElementById("row")
-function createCard(jsondeck, cardNum){
+function createCard(jsondeck, ranNum){
     const flipContainer = document.createElement('div');
-    flipContainer.setAttribute('class','col-3 flip-container');
-    flipContainer.setAttribute('onclick','this.classList.toggle(\'clicked\');')
+    let name = jsondeck[ranNum].name
+    flipContainer.setAttribute('class',`col-3 flip-container ${name}`);
+    flipContainer.setAttribute('onclick','this.classList.toggle(\'clicked\');');
     const flipper = document.createElement('div');
-    flipper.setAttribute('class','flipper flipcard border border-dark rounded')
-    flipper.setAttribute('class', jsondeck[cardNum].name)
+    flipper.setAttribute('class',`flipper flipcard border border-dark rounded`)
     const front = document.createElement('div');
     front.setAttribute('class','front details rounded')
     const frontImage = document.createElement('div');
@@ -31,7 +31,7 @@ function createCard(jsondeck, cardNum){
     const backImage = document.createElement('div');
     backImage.setAttribute('class','backImage')
     const img = document.createElement('img')
-    img.src = jsondeck[cardNum].img;
+    img.src = jsondeck[ranNum].img;
     backImage.appendChild(img)
     //backImage.innerHtml is image (unique image)
     back.appendChild(backImage);
@@ -41,19 +41,22 @@ function createCard(jsondeck, cardNum){
     row.appendChild(flipContainer)
 }
 
-function countOccurrence(arr,val){
-    return arr.filter(v=>(v===val)).length
+// function countOccurrence(arr,val){
+//     return arr.filter(v=>(v===val)).length
+// }
+
+const orderedDeck = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13]
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
 }
 function buildDeck(json){
-    console.log(json)
-    let deck = [];
-    while(deck.length<27){
-        let rando = Math.random() * (15);
-        if(countOccurrence(deck,rando)<2){
-            deck.push(rando)
-            createCard(json, rando)
-        }
-    }
+    let gameDeck = shuffleArray(orderedDeck)
+    gameDeck.map(card=>createCard(json,card))
 }
 
 fetch(letters)
